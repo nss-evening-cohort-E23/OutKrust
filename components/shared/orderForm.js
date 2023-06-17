64efb6d3df67546b2806686fdab838646db67be4
Michael Nunan <michael.nunan@outlook.com>
2023-06-17 12:06:19 -0500

    fixed all object literals to match firebase key syntax- snake case

--- components/shared/orderForm.js
+++ components/shared/orderForm.js
@@ -6,22 +6,22 @@ const orderForm = (obj = {}) => {
 <form id="order" class="mb-4">
   <div class="form-group">
     <label for="order-name">Order Name</label>
-    <input type="text" class="form-control" id="order-name" name="order-name" value="${obj.orderName || ''}" required>
+    <input type="text" class="form-control" id="order-name" name="order-name" value="${obj.name || ''}" required>
   </div>
   <div class="form-group">
     <label for="order-phone">Customer Phone</label>
-    <input type="text" class="form-control" id="order-phone" name="order-phone" value="${obj.orderPhone || ''}" required>
+    <input type="text" class="form-control" id="order-phone" name="order-phone" value="${obj.customer_phone || ''}" required>
   </div>
   <div class="form-group">
     <label for="order-email">Customer Email</label>
-    <input type="text" class="form-control" id="order-email" name="order-email" value="${obj.orderEmail || ''}" required>
+    <input type="text" class="form-control" id="order-email" name="order-email" value="${obj.customer_email || ''}" required>
   </div>
   <div class="form-group">
     <label for="order-type">Order Type</label>
     <select id="order-type" name="order-type" required>
-      <option value="Dine-In" name="dine-in" ${obj.orderType === 'Dine-in' ? 'selected' : ''}>Dine-In</option>
-      <option value="Pick-Up" name="pick-up" ${obj.orderType === 'Pick-up' ? 'selected' : ''}>Pick-Up</option>
-      <option value="Delivery" name="delivery" ${obj.orderType === 'Delivery' ? 'selected' : ''}>Delivery</option>
+      <option value="Dine-In" name="dine-in" ${obj.order_type === 'Dine-in' ? 'selected' : ''}>Dine-In</option>
+      <option value="Pick-Up" name="pick-up" ${obj.order_type === 'Pick-up' ? 'selected' : ''}>Pick-Up</option>
+      <option value="Delivery" name="delivery" ${obj.order_type === 'Delivery' ? 'selected' : ''}>Delivery</option>
     </select>
   </div>
   <button type="submit" class="form-btn" id="submitOrder">Submit Order</button>
--- pages/cards.js
+++ pages/cards.js
@@ -7,11 +7,11 @@ const showCards = (array) => {
     domString += `
     <div class="card" style="width: 18rem;">
       <div class="card-body">
-        <h5 class="card-name">${order.orderName}</h5>
-        <h6 class="card-phone">${order.customerPhone}</h6>
-        <h7 class="card-email">${order.customerEmail}</h7>
-        <h8 class="card-order-type">${order.orderType}</h8>
-        <a href="#" class="card-link" id="detail-entry--${order.firebaseKey}">Create/Edit Order</a>
+        <h5 class="card-name">${order.name}</h5>
+        <h6 class="card-phone">${order.customer_phone}</h6>
+        <h7 class="card-email">${order.customer_email}</h7>
+        <h8 class="card-order-type">${order.order_type}</h8>
+        <a href="#" class="card-link" id="detail-entry--${order.order_number}">Create/Edit Order</a>
       </div>
     </div>
   `;
