565a192f8fe55b8a4fafce1791471d5036867595
Mara Caoile <mara.caoile@gmail.com>
2023-06-17 13:02:52 -0500

    updated payment events, created item data, update home page and updated items.js

---
+++ api/itemData.js
@@ -0,0 +1,83 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+// ALL ITEMS //
+
+const getAllItems = () => new Promise((resolve, reject) => {
+  // fetch is the function that has two parameters
+  fetch(`${endpoint}/items.json`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    // then is a function inside promise, input is of type Response
+    // response and data are taco
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+        // creates an array of values from the object
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// GET SINGLE ITEM //
+
+const getSingleItem = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/items/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// UPDATE ITEMS //
+
+const updateItem = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/items/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+// DELETE ITEMS //
+
+const deleteItems = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/items/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+export {
+  getAllItems,
+  getSingleItem,
+  updateItem,
+  deleteItems
+};
---
+++ pages/items.js
@@ -0,0 +1,35 @@
+import { getAllItems } from '../api/itemData';
+import renderToDom from '../utils/renderToDom';
+
+const itemSum = (items) => {
+  let totalItems = 0;
+  items.forEach((item) => {
+    totalItems += item.price;
+  }); getAllItems().then(totalItems);
+};
+
+const totalString = `<h1>TOTAL: ${itemSum}</h1>`;
+renderToDom('#homePage', totalString);
+
+const showItems = (array) => {
+  let cardString = '';
+  array.forEach((item) => {
+    cardString += `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${item.name}</h5>
+      <h6 class="lang">${item.price}</h6>
+        <hr>
+        <button id="editItemBtn--${item.firebaseKey}" class="btn btn-info">edit</button>
+        <button id="deleteItemBtn--${item.firebaseKey}" class="btn btn-danger">delete</button>
+    </div>
+  </div>
+  `;
+
+    const btnString = '<button class="new-card-btn" id="addItemBtn">Add Item</button> <button class="new-card-btn" id="goPayBtn">Go To Payment</button>';
+    renderToDom('#createWordBtn', btnString);
+  });
+  renderToDom('#homePage', cardString);
+};
+
+export default showItems;
