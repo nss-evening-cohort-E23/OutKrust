5eef8099650fceeb1e487658b4d832e09904b6a6
melissapennington <mlpennington9@gmail.com>
2023-06-20 12:33:23 -0500

    modifications

--- pages/cards.js
+++ pages/cards.js
@@ -5,21 +5,29 @@ const orderCards = (array) => {
   array.forEach((order) => {
     console.warn('order', order);
     domString += `
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-name">${order.name}</h5>
-        <h6 class="card-phone">${order.customer_phone}</h6>
-        <h7 class="card-email">${order.customer_email}</h7>
-        <h8 class="card-order-type">${order.order_type}</h8>
-        <a href="#" class="card-link" id="detail-entry--${order.order_number}">Detail</a>
-        <a href="#" class="card-link" id="update-entry--${order.order_number}">Edit</a>
-        <a href="#" class="card-link" id="delete-entry--${order.order_number}">Delete</a>
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-name">${order.name}</h5>
+          <h6 class="card-phone">${order.customer_phone}</h6>
+          <h7 class="card-email">${order.customer_email}</h7>
+          <h8 class="card-order-type">${order.order_type}</h8>
+          <a href="#" class="card-link" id="detail-entry--${order.order_number}" class="btn btn-success">Detail</a>
+          <a href="#" class="card-link" id="update-entry--${order.order_number}" class="btn btn-warning">Edit</a>
+          <a href="#" class="card-link" id="delete-entry--${order.order_number}" class="btn btn-danger">Delete</a>
+        </div>
       </div>
-      <div></div>
-    </div>
-  `;
+    `;
   });
-  renderToDom('#homePage', domString);
+
+  const containerStyle = `
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  `;
+  const containerElement = document.createElement('div');
+  containerElement.setAttribute('style', containerStyle);
+  containerElement.innerHTML = domString;
+  renderToDom('#homePage', domString, containerElement.outerHTML);
 };
 
 export default orderCards;
